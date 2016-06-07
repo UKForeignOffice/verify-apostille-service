95d0c21363bfd82e5f3971d42250161bdccc448c
John Shields <john.shields@informed.com>
2016-06-07 14:24:34 +0100

    FCOVAS-56: Proof of concept for replacement Verify Apostille Service.

---
+++ verifyApostille/api/controllers/Verify-apostilleController.js
@@ -0,0 +1,11 @@
+/**
+ * Verify-apostilleController
+ *
+ * @description :: Server-side logic for managing verify-apostilles
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
