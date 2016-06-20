94c5f06aa5012f37afdff346e616c81a45e2302d
John Shields <john.shields@informed.com>
2016-06-20 16:52:46 +0100

    FCOVAS-56: Add extra validation for date and apostille number combinations.

--- verifyApostille/api/controllers/VerifyApostilleController.js
+++ verifyApostille/api/controllers/VerifyApostilleController.js
@@ -5,22 +5,28 @@
 
 var apostilleDetailsController = {
     openApostillePage: function(req,res){
-        return res.view('verifyApostille.ejs',{error_report:false});
+        return res.view('verifyApostille.ejs',{error_report:false, apost_number:false, apost_dd:false, apost_mm:false, apost_yyyy:false});
     },
 
     findApostille: function(req, res) {
             console.log("APOSTILLE NUMBER input RES: ", req.body);
         var errors = [];
 
-        if(!req.body.ApostDay || !req.body.ApostMonth || !req.body.ApostYear ){
+         if(!req.body.ApostDay.match(/\d{1,2}/) || !req.body.ApostMonth.match(/\d{1,2}/) || !req.body.ApostYear.match(/\d{4}/) ){
             errors.push({link:"date-container", message:"Unfortunately we can't validate the Apostille date entered, please check that the date format is correct"})
-        }    
+        }
 
-        if(!req.body.ApostNumber || (req.body.ApostNumber && req.body.ApostNumber.length<8)){
+        if(!req.body.ApostNumber.match(/^[a-zA-Z]\d{6,7}/)){
             errors.push({link:"ApostNumber", message:"Please enter the complete Apostille number"})
         }
         if(errors.length > 0){
-            return res.view('verifyApostille.ejs',{error_report : errors});
+            return res.view('verifyApostille.ejs',{
+                error_report : errors,
+                apost_number : req.body.ApostNumber,
+                apost_dd : req.body.ApostDay,
+                apost_mm: req.body.ApostMonth,
+                apost_yyyy: req.body.ApostYear
+            });
         }
 
         VerifyApostilleDetails.findOne({
--- verifyApostille/api/models/VerifyApostilleDetails.js
+++ verifyApostille/api/models/VerifyApostilleDetails.js
@@ -4,30 +4,17 @@
 */
 var verifyApostilleDetails = {
 
-
-  // I don't want createdAt
-  //   createdAt: false,
-  //
-  //     // I want updatedAt to actually be called updateTimestamp
-  //       updatedAt: 'updateTimestamp'
-  //
-  //         // And deletedAt to be called destroyTime (remember to enable paranoid for this to work)
-  //           deletedAt: 'destroyTime',
-  //             paranoid: true
-
-
     timestamps: false,
 
     createdAt: false,
     updatedAt: false,
-
 
     attributes: {
         ApostilleNumber: {
             type: Sequelize.STRING
         },
         DateIssued: {
-            type: Sequelize.STRING
+            type: Sequelize.DATE
         },    
         SignedBy: {
             type: Sequelize.STRING
