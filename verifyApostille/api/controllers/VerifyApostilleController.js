e6f2c164b0ed6752a6bddca9abe266fd0a3b6bdb
rp825 <rp825@york.ac.uk>
2016-08-22 10:40:37 +0100

    Second test

--- verifyApostille/api/controllers/VerifyApostilleController.js
+++ verifyApostille/api/controllers/VerifyApostilleController.js
@@ -42,7 +42,7 @@ var apostilleDetailsController = {
         console.log("APOSTILLE NUMBER input RES: ", req.body);
         var errors = [];
 
-         if(!req.body.ApostDay.match(/\d{1,2}/) || !req.body.ApostMonth.match(/\d{1,2}/) || !req.body.ApostYear.match(/\d{4}/) ){
+        if(!req.body.ApostDay.match(/\d{1,2}/) || !req.body.ApostMonth.match(/\d{1,2}/) || !req.body.ApostYear.match(/\d{4}/) ){
             errors.push({link:"date-container", message:"Enter a valid date"})
         }
 
@@ -78,6 +78,8 @@ var apostilleDetailsController = {
         var startDate = req.body.ApostDay + "-" + req.body.ApostMonth + "-" + req.body.ApostYear + " 00:00:00";
         var endDate = req.body.ApostDay + "-" + req.body.ApostMonth + "-" + req.body.ApostYear + " 23:59:59";
 
+
+        console.log(req.body);
         VerifyApostilleDetails.findOne({
             where: {
                 ApostilleNumber: req.body.ApostNumber,
@@ -107,29 +109,29 @@ var apostilleDetailsController = {
                 IssuedBy: req.session.IssuedBy
             });
         })
-        .catch( function(error) {
-            sails.log(error);
-            console.log(error);
-            req.session.errCnt++;
+            .catch( function(error) {
+                sails.log(error);
+                console.log(error);
+                req.session.errCnt++;
 
-            errors = [];
-            errors.push({link:"date-container", message:"Check the date."});
+                errors = [];
+                errors.push({link:"date-container", message:"Check the date."});
                 errors.push({link:"ApostNumber", message:"Check the apostille number."});
-            if (req.session.errCnt > 2 ){
-                errors.push({
-                    link:"ApostNumber",
-                    message: "Please email verifyapostille@fco.gov.uk with the Apostille number and date."
-                });
-            }
+                if (req.session.errCnt > 2 ){
+                    errors.push({
+                        link:"ApostNumber",
+                        message: "Please email verifyapostille@fco.gov.uk with the Apostille number and date."
+                    });
+                }
 
-            return res.view('verifyApostille.ejs', {
-                error_report: errors,
-                apost_number : req.body.ApostNumber,
-                apost_dd : req.body.ApostDay,
-                apost_mm: req.body.ApostMonth,
-                apost_yyyy: req.body.ApostYear
+                return res.view('verifyApostille.ejs', {
+                    error_report: errors,
+                    apost_number : req.body.ApostNumber,
+                    apost_dd : req.body.ApostDay,
+                    apost_mm: req.body.ApostMonth,
+                    apost_yyyy: req.body.ApostYear
+                });
             });
-        });
     }
 };
 module.exports = apostilleDetailsController;
--- verifyApostille/test/unit/controllers/VerifyApostilleController.test.js
+++ verifyApostille/test/unit/controllers/VerifyApostilleController.test.js
@@ -3,17 +3,41 @@ var request = require('supertest'),
 
 
 describe('VerifyApostilleController', function() {
+    var user
+
     describe('#healthCheck()', function() {
         it('should return health check JSON', function (done) {
-            var user = agent(sails.hooks.http.app);
+            user = agent(sails.hooks.http.app);
             user
                 .get('/healthcheck')
                 .expect(200)
                 .end(function(err, res) {
                     res.body.message.should.equal('verify-apostille-service running');
                     done();
-
                 });
+        });
+    });
+
+
+    describe('#openApostillePage()', function() {
+        it('should return apostille page view', function (done) {
+            user
+                .get('/verify')
+                .expect(200,done);
+        });
+    });
+
+    describe('#findApostille()', function() {
+        it('should return apostille page view', function (done) {
+            user
+                .post('/details')
+                .send({
+                    ApostNumber : 'APO-1',
+                    ApostDay : '11',
+                    ApostMonth : '07',
+                    ApostYear : '2016'
+                })
+                .expect(200,done);
         });
     });
 
