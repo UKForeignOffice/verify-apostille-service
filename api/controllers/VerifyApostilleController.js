7117b553ef6d6219a4e98e70642ce52968ea3610
Benjamin Dickey <43062503+benjamindi96k@users.noreply.github.com>
2022-02-01 10:37:12 +0000

    Revert logging

--- api/controllers/VerifyApostilleController.js
+++ api/controllers/VerifyApostilleController.js
@@ -44,11 +44,7 @@ var apostilleDetailsController = {
 
     findApostille: function(req, res) {
         // When the trust proxy setting is true, the value of this property is derived from the left-most entry in the X-Forwarded-For header.
-        console.log("REQUEST IP: "  + req.ip);
-        // When the trust proxy setting is true, this property contains an array of IP addresses specified in the X-Forwarded-For request header.
-        console.log("REQUEST IPs: "  + req.ips);
-        // X-Forwarded-For should be automatically set when using an AWS load balancer
-        console.log("REQUEST HEADER: " + req.headers["x-forwarded-for"]);
+        console.log("Got apostille request", req.body);
 
         var errors = [];
 
