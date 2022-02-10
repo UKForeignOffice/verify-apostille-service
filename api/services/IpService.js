34bdfbf2b4bb17a0a99f97a975ca558face16f5a
Benjamin Dickey <43062503+benjamindi96k@users.noreply.github.com>
2022-02-10 12:55:28 +0000

    Clear ip log daily

--- api/services/IpService.js
+++ api/services/IpService.js
@@ -2,8 +2,20 @@ var ipLog = {};
 const maxFailedAttempts = 15;
 
 var IpService = {
+
+    clearIpLogIfNewDay: function() {
+        var datetime = new Date();
+        var day = datetime.getDay();
+
+        if(ipLog.day != day) {
+            ipLog = {};
+            ipLog.day = day;
+        }
+    },
     
     storeIp: function(ip, lookupSuccess){
+        this.clearIpLogIfNewDay();
+
         if(ip == null || lookupSuccess) return;
 
         if(ipLog[ip] == null){
