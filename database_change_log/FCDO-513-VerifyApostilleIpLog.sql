e13c39bc63171feb1029c03db97586973eb57915
Benjamin Dickey <benjamindickey@hotmail.co.uk>
2022-03-23 10:55:18 +0000

    Change delete to truncate

--- database_change_log/FCDO-513-VerifyApostilleIpLog.sql
+++ database_change_log/FCDO-513-VerifyApostilleIpLog.sql
@@ -4,8 +4,8 @@ CREATE TABLE IF NOT EXISTS public."VerifyApostilleIpLog"
     "FailedAttempts" integer,
     "BlockedAt" varchar(27),
     CONSTRAINT "VerifyApostilleIpLog_pkey" PRIMARY KEY ("Ip")
-)
+);
 
 -- Without FirstFailedAttemptAt, existing rows will not update this column, so we must drop existing rows
-delete from "VerifyApostilleIpLog";
+TRUNCATE "VerifyApostilleIpLog";
 ALTER TABLE "VerifyApostilleIpLog" ADD COLUMN "FirstFailedAttemptAt" varchar(27);
